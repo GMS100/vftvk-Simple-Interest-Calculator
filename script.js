60b45b5845159f0f5961bd0a880723a85c45c781
Gina <2021gsonda@gmail.com>
2022-04-05 21:56:23 +0000

    changes thru exercise 4 step 12

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //create variables for data from form
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    //calculate the interest earned
+    var interest = (principal * years * rate)/100;
+
+    //identify the end year 
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML=
+    "If you deposit " + principal + ", \<br\>" +
+    "at an interest rate of " + rate + "%. \<br\>" +
+    "You will receive an amount of " + interest + ", \<br\>" +
+    "in the year " + year + "\<br\>"
+}
+
+function updateRate()
+{
+    //retrieve the value selected by the slider
+    var rateval = document.getElementById("rate").value;
+
+    //set display to the value from slider
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
         
